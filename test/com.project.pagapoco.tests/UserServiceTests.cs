3c1b856539d3419beb83907f1d0a2c79b19cccd9
Diego <taysonm895@gmail.com>
2025-06-23 02:43:35 -0300

    Integracion de test

--- test/com.project.pagapoco.tests/UserServiceTests.cs
+++ test/com.project.pagapoco.tests/UserServiceTests.cs
@@ -1,5 +1,4 @@
-﻿using com.project.pagapoco.core.business;
-using com.project.pagapoco.core.business.Service;
+﻿using com.project.pagapoco.core.business.Service;
 using com.project.pagapoco.core.data.Repository;
 using com.project.pagapoco.core.entities;
 using Moq;
@@ -23,39 +22,54 @@ namespace com.project.pagapoco.tests
         }
 
         [Fact]
-        public void DebeBuscarUnUsuarioPorSuId()
+        public async Task DebeBuscarUnUsuarioExistentePorSuDni()
         {
 
             // Arrange / Preparar
 
             // Crea un usuario de ejemplo que será devuelto por el mock
-            var idUsuario = 1;
+            //var idUser = 1;
+            var dniUsuario = 44556622;
             var usuarioEsperado = new User
             {
-                Id = idUsuario,
+                //Id = idUser,
+                Dni = dniUsuario,
                 FirstName = "Lionel",
                 LastName = "Messi",
                 Email = "correodemessi123@gmail.com",
                 Password = "12356"
             };
 
-            // Configura el mock para que devuelva el usuario esperado cuando se llame a FindById
+            // Configura el mock para que devuelva el usuario esperado cuando se llame a FindByDni
             _mockUserRepository
-                .Setup(x => x.FindById(idUsuario))
-                .Returns(usuarioEsperado);
+                .Setup(x => x.FindByDni(dniUsuario))
+                .ReturnsAsync(usuarioEsperado);
 
             // Act / Ejecutar
-            var usuarioObtenido = _userService.findById(idUsuario);
+            var usuarioObtenido = await _userService.GetUserByDni(dniUsuario);
 
             // Assert / Verificar
             Assert.NotNull(usuarioObtenido);
-            Assert.Equal(usuarioEsperado.Id, usuarioObtenido.Id);
+            Assert.Equal(usuarioEsperado.Dni, usuarioObtenido.Dni);
             Assert.Equal(usuarioEsperado.FirstName, usuarioObtenido.FirstName);
 
             // Verifica que el método FindById fue llamado una vez con el id correcto
-            _mockUserRepository.Verify(x => x.FindById(idUsuario), Times.Once);
+            _mockUserRepository.Verify(x => x.FindByDni(dniUsuario), Times.Once);
 
+        }
 
+        [Fact]
+        public async Task DebeBuscarUnUsuarioQueNoExisteYLanzarErrorPorSuDni()
+        {
+            var dniIncorrecto = 11111111;
+            _mockUserRepository
+                .Setup(x => x.FindByDni(dniIncorrecto))
+                .ReturnsAsync((User)null);
+
+            var resultado = await _userService.GetUserByDni(dniIncorrecto);
+
+            Assert.Null(resultado);
+            _mockUserRepository.Verify(x => x.FindByDni(dniIncorrecto), Times.Once);
         }
     }
 }
